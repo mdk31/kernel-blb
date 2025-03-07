4b16eef4ddd91b4efb554752ba472229b6c22cec
Matthew Kosko <matthewdkosko@gmail.com>
2025-03-06 20:09:41 -0500

    Added kangschafer dgp function

--- code/helper_functions.R
+++ code/helper_functions.R
@@ -1,5 +1,20 @@
 
 
-kangschafer3 <- function()
+kangschafer3 <- function(){
+  X1 <- rnorm(n, 0, 1)
+  X2 <- rnorm(n, 0, 1)
+  prt <- 1/(1 + exp(-beta_overlap*(X1 + X2)))
+  
+  Tr <- rbinom(n, 1, prt)
+  
+  Y0  <- X1 + X2 + rnorm(n, 0, sigma)
+  Y1  <- Y0 + te
+  y   <- Y0*(1-Tr) + Y1*(Tr)
+  out <- cbind(y, Tr, X1, X2, Y1, Y0)
+  out <- data.table::as.data.table(out)
+  return(out)
+}
 
-causalblb <- function()
+causalblb <- function(){
+  
+}
