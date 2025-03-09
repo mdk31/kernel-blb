6e25fb188a6f8f1087bde63251f4473f14bce4c3
Matthew Kosko <matthewdkosko@gmail.com>
2025-03-09 12:18:41 -0400

    Commented out full bootstrap on AIPW, not sure its needed

--- code/simulations/aipw.R
+++ code/simulations/aipw.R
@@ -39,44 +39,44 @@ if(!file.exists(img_tmp_dir)){
 n_values <- c(10000)
 subset_values <- c(5, 10, 15)
 
-# FULL SIMULATIONS----
-grid_vals <- as.data.table(expand.grid(n = n_values))
-seq_row <- seq_len(nrow(grid_vals))
-
-if(file.exists(file.path(temp_dir, 'full_bootstrap.rds'))){
-  cblb <- readRDS(file.path(temp_dir, 'full_bootstrap.rds'))
-} else{
-  cblb <- lapply(seq_row, function(i){
-    grid_val <- grid_vals[i]
-    n <- grid_val$n
-
-    out <- pblapply(seq_len(replications), function(rp){
-      set.seed(rp)
-      dat <- kangschafer3(n = n, te = te, sigma = sigma, beta_overlap = 0.5)
-      output <- aipw_kernel_weights(dat,degree1,degree2,k1,k2,operator,penal)
-      phi1 <- output$phi1
-      phi0 <- output$phi0
-      M <- rmultinom(n = B, size = n, prob = rep(1, n))
-      boot_reps <- sapply(seq_len(B), function(bt){
-        boot_phi1 <- M[, bt]*phi1
-        boot_phi0 <- M[, bt]*phi0
-        mean(boot_phi1) - mean(boot_phi0)
-      })
-      
-      perc_ci <- boot:::perc.ci(boot_reps)
-      return(data.table(lower_ci = perc_ci[4],
-                        upper_ci = perc_ci[5],
-                        estim = mean(boot_reps),
-                        se = sd(boot_reps)))
-    }, cl = 1)
-    out <- rbindlist(out)
-    out[, `:=`(n = n)]
-    out
-  })
-  cblb <- rbindlist(cblb)
-  saveRDS(cblb, file.path(temp_dir, 'full_bootstrap.rds'))
-}
-
+# # FULL SIMULATIONS----
+# grid_vals <- as.data.table(expand.grid(n = n_values))
+# seq_row <- seq_len(nrow(grid_vals))
+# 
+# if(file.exists(file.path(temp_dir, 'full_bootstrap.rds'))){
+#   cblb <- readRDS(file.path(temp_dir, 'full_bootstrap.rds'))
+# } else{
+#   cblb <- lapply(seq_row, function(i){
+#     grid_val <- grid_vals[i]
+#     n <- grid_val$n
+# 
+#     out <- pblapply(seq_len(replications), function(rp){
+#       set.seed(rp)
+#       dat <- kangschafer3(n = n, te = te, sigma = sigma, beta_overlap = 0.5)
+#       output <- aipw_kernel_weights(dat,degree1,degree2,k1,k2,operator,penal)
+#       phi1 <- output$phi1
+#       phi0 <- output$phi0
+#       M <- rmultinom(n = B, size = n, prob = rep(1, n))
+#       boot_reps <- sapply(seq_len(B), function(bt){
+#         boot_phi1 <- M[, bt]*phi1
+#         boot_phi0 <- M[, bt]*phi0
+#         mean(boot_phi1) - mean(boot_phi0)
+#       })
+#       
+#       perc_ci <- boot:::perc.ci(boot_reps)
+#       return(data.table(lower_ci = perc_ci[4],
+#                         upper_ci = perc_ci[5],
+#                         estim = mean(boot_reps),
+#                         se = sd(boot_reps)))
+#     }, cl = 1)
+#     out <- rbindlist(out)
+#     out[, `:=`(n = n)]
+#     out
+#   })
+#   cblb <- rbindlist(cblb)
+#   saveRDS(cblb, file.path(temp_dir, 'full_bootstrap.rds'))
+# }
+# 
 
 # cBLB SIMULATIONS----
 grid_vals <- as.data.table(expand.grid(n = n_values,
