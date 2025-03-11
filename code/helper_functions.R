5bf2a863ee21525174d31b2f0d15779fe035d195
Matthew Kosko <matthewdkosko@gmail.com>
2025-03-11 09:43:32 -0400

    Updated causal blb function for stable balancing

--- code/helper_functions.R
+++ code/helper_functions.R
@@ -218,7 +218,7 @@ causal_blb_aipw <- function(data, b, subsets,  degree1, degree2, k1, k2, operato
   return(blb_out)
 }
 
-causal_blb_stable <- function(data, b, subsets,  degree1, degree2, k1, k2, operator, penal, disjoint = TRUE){
+causal_blb_stable <- function(data, b, subsets, kernel_approx = TRUE, disjoint = TRUE){
   if(data.table::is.data.table(data) == FALSE){
     data <- data.table::as.data.table(data)
   }
@@ -228,11 +228,19 @@ causal_blb_stable <- function(data, b, subsets,  degree1, degree2, k1, k2, opera
   
   blb_out <- lapply(partitions, function(i){
     tmp_dat <- data[i]
-    output <- aipw_kernel_weights(tmp_dat, degree1, degree2, k1, k2, operator, penal)
-    phi1 <- output$phi1
-    phi0 <- output$phi0
+    output <- osqp_kernel_sbwATE(X = as.matrix(tmp_dat[, c('X1', 'X2')]),
+                                                   A = tmp_dat$Tr,
+                                                   Y = tmp_dat$y,
+                                                   delta.v=1e-4,
+                                                   kernel.approximation = kernel_approx,
+                                                   c = 100)
     
+    tmp_dat$full_weights <- output[[1]]$res$x*b
+    
+    phi1 <- (tmp_dat$Tr)*tmp_dat$full_weights*(tmp_dat$y - m1) + m1
+    phi0 <- (1-tmp_dat$Tr)*tmp_dat$full_weights*(tmp_dat$y - m0) + m0
     M <- rmultinom(n = B, size = n, prob = rep(1, b))
+    
     blb_reps <- sapply(seq_len(B), function(bt){
       boot_phi1 <- M[, bt]*phi1
       boot_phi0 <- M[, bt]*phi0
