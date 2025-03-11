9690d55bbc71104a6bb0985189ed4c46015d4bd5
Matthew Kosko <matthewdkosko@gmail.com>
2025-03-11 09:39:29 -0400

    Updated stable balancing file

--- code/simulations/stable_balancing.R
+++ code/simulations/stable_balancing.R
@@ -1,15 +1,14 @@
 library(pbapply)
 library(data.table)
 
-# TODO: install kernel packages
-source('code/helper_functions.R')
+source('code/helper_funcs.R')
+source('code/osqp-kernel-sbw.R')
+Rcpp::sourceCpp("code/RBF_kernel_C_parallel.cpp")
 
 te <- 0.8
 sigma <- 1
 replications <- 1000
 B <- 100 #bootstrap resamples
-optimal_val <- 1.001 #extracted from PAPER
-
 
 base_nm <- 'stable_balancing'
 image_path <- 'images'
@@ -31,41 +30,41 @@ n_values <- c(10000)
 subset_values <- c(5, 10, 15)
 
 # FULL SIMULATIONS----
-grid_vals <- as.data.table(expand.grid(n = n_values))
-seq_row <- seq_len(nrow(grid_vals))
-
-if(file.exists(file.path(temp_dir, 'full_bootstrap.rds'))){
-  cblb <- readRDS(file.path(temp_dir, 'full_bootstrap.rds'))
-} else{
-  cblb <- lapply(seq_row, function(i){
-    grid_val <- grid_vals[i]
-    n <- grid_val$n
-
-    out <- pblapply(seq_len(replications), function(rp){
-      set.seed(rp)
-      dat <- aol_dgp(n = n)
-      lambda <- 0.01
-      initial_params <- c(rep(0, n), 0)  # Initial v and b
-      estim_opt_regime <- estimate_optimal_regime(data, initial_params, lambda) 
-      M <- rmultinom(n = B, size = n, prob = rep(1, n))
-      
-      boot_reps <- sapply(seq_len(B), function(bt){
-        sum(M[, bt]*dat$y/0.5*(dat$A == estim_opt_regime))/n
-      })
-      
-      perc_ci <- boot:::perc.ci(boot_reps)
-      return(data.table(lower_ci = perc_ci[4],
-                        upper_ci = perc_ci[5],
-                        estim = mean(boot_reps),
-                        se = sd(boot_reps)))
-    }, cl = 4)
-    out <- rbindlist(out)
-    out[, `:=`(n = n)]
-    out
-  })
-  cblb <- rbindlist(cblb)
-  saveRDS(cblb, file.path(temp_dir, 'full_bootstrap.rds'))
-}
+# grid_vals <- as.data.table(expand.grid(n = n_values))
+# seq_row <- seq_len(nrow(grid_vals))
+# 
+# if(file.exists(file.path(temp_dir, 'full_bootstrap.rds'))){
+#   cblb <- readRDS(file.path(temp_dir, 'full_bootstrap.rds'))
+# } else{
+#   cblb <- lapply(seq_row, function(i){
+#     grid_val <- grid_vals[i]
+#     n <- grid_val$n
+# 
+#     out <- pblapply(seq_len(replications), function(rp){
+#       set.seed(rp)
+#       dat <- aol_dgp(n = n)
+#       lambda <- 0.01
+#       initial_params <- c(rep(0, n), 0)  # Initial v and b
+#       estim_opt_regime <- estimate_optimal_regime(data, initial_params, lambda) 
+#       M <- rmultinom(n = B, size = n, prob = rep(1, n))
+#       
+#       boot_reps <- sapply(seq_len(B), function(bt){
+#         sum(M[, bt]*dat$y/0.5*(dat$A == estim_opt_regime))/n
+#       })
+#       
+#       perc_ci <- boot:::perc.ci(boot_reps)
+#       return(data.table(lower_ci = perc_ci[4],
+#                         upper_ci = perc_ci[5],
+#                         estim = mean(boot_reps),
+#                         se = sd(boot_reps)))
+#     }, cl = 4)
+#     out <- rbindlist(out)
+#     out[, `:=`(n = n)]
+#     out
+#   })
+#   cblb <- rbindlist(cblb)
+#   saveRDS(cblb, file.path(temp_dir, 'full_bootstrap.rds'))
+# }
 
 
 # cBLB SIMULATIONS----
@@ -82,7 +81,7 @@ if(file.exists(file.path(temp_dir, 'cblb_bootstrap.rds'))){
     n <- grid_val$n
     subsets <- grid_val$subsets
     gamma <- grid_val$gamma
-    b <- round(n^gamma)
+    b <- floor(n^gamma)
     
     out <- pblapply(seq_len(replications), function(rp){
       set.seed(rp)
