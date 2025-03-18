b2aef89286383d04dc4be66b30ad18e52693d1af
Matthew Kosko <matthewdkosko@gmail.com>
2025-03-18 15:05:21 -0400

    Added timing code for stable balancing code

--- code/timing/stable_balancing_noaugmentation.R
+++ code/timing/stable_balancing_noaugmentation.R
@@ -6,10 +6,10 @@ Rcpp::sourceCpp("code/RBF_kernel_C_parallel.cpp")
 
 te <- 0.8
 sigma <- 1
-replications <- 1000
+replications <- 25
 B <- 100 # bootstrap resamples
 
-base_nm <- 'stable_balancing_noaugmentation'
+base_nm <- 'stable_balancing_noaugmentation_timing'
 image_path <- 'images'
 dat_path <- 'data'
 
@@ -25,12 +25,12 @@ if(!file.exists(img_tmp_dir)){
 }
 
 # Values for simulations
-n_values <- c(5000)
+n_values <- c(100000)
 subset_values <- c(5, 10)
 
 # FULL SIMULATIONS----
 grid_vals <- as.data.table(expand.grid(n = n_values, 
-                                       kernel_approx = c(FALSE),
+                                       kernel_approx = c(TRUE),
                                        kernel_type = c('rbfdot', 'vanilladot')))
 grid_vals <- grid_vals[!(kernel_type == 'vanilladot' & kernel_approx)]
 seq_row <- seq_len(nrow(grid_vals))
@@ -52,38 +52,35 @@ if(file.exists(file.path(temp_dir, 'full_bootstrap.rds'))){
     out <- pblapply(seq_len(replications), function(rp){
       set.seed(rp)
       dat <- kangschafer3(n = n, te = te, sigma = sigma, beta_overlap = 0.5)
-      output <- osqp_kernel_sbw_twofit(X = as.matrix(dat[, c('X1', 'X2')]),
-                                       A = dat$Tr,
-                                       Y = dat$y,
-                                       delta.v=1e-4,
-                                       kernel.approximation = kernel_approx,
-                                       c = 250, kernel_type = kernel_type, eig_clip = eig_clip)
-      browser()
-      
-      weights0 <- output[[1]]$res0$x
-      weights1 <- output[[1]]$res1$x
-      dat$full_weights <- NA
-      dat$full_weights[dat$Tr == 1] <- weights1
-      dat$full_weights[dat$Tr == 0] <- weights0
-      dat$full_weights <- dat$full_weights*n
-      # dat$full_weights <- output[[1]]$res$x*n
-      
-      phi1 <- (dat$Tr)*dat$full_weights*(dat$y)
-      phi0 <- (1-dat$Tr)*dat$full_weights*(dat$y)
-      M <- rmultinom(n = B, size = n, prob = rep(1, n))
-      
-      blb_reps <- sapply(seq_len(B), function(bt){
-        boot_phi1 <- M[, bt]*phi1
-        boot_phi0 <- M[, bt]*phi0
-        mean(boot_phi1) - mean(boot_phi0)
+      time <- system.time({
+        output <- osqp_kernel_sbw_twofit(X = as.matrix(dat[, c('X1', 'X2')]),
+                                         A = dat$Tr,
+                                         Y = dat$y,
+                                         delta.v=1e-4,
+                                         kernel.approximation = kernel_approx,
+                                         kernel_type = kernel_type, eig_clip = eig_clip)
         
+        weights0 <- output[[1]]$res0$x
+        weights1 <- output[[1]]$res1$x
+        dat$full_weights <- NA
+        dat$full_weights[dat$Tr == 1] <- weights1
+        dat$full_weights[dat$Tr == 0] <- weights0
+        dat$full_weights <- dat$full_weights*n
+        # dat$full_weights <- output[[1]]$res$x*n
+        
+        phi1 <- (dat$Tr)*dat$full_weights*(dat$y)
+        phi0 <- (1-dat$Tr)*dat$full_weights*(dat$y)
+        M <- rmultinom(n = B, size = n, prob = rep(1, n))
+        
+        blb_reps <- sapply(seq_len(B), function(bt){
+          boot_phi1 <- M[, bt]*phi1
+          boot_phi0 <- M[, bt]*phi0
+          mean(boot_phi1) - mean(boot_phi0)
+        })
+        perc_ci <- boot:::perc.ci(blb_reps)
       })
       
-      perc_ci <- boot:::perc.ci(blb_reps)
-      return(data.table(lower_ci = c(perc_ci[4]),
-                        upper_ci = c(perc_ci[5]),
-                        estim = c(mean(blb_reps)),
-                        se = c(sd(blb_reps))))
+      return(data.table(time = time['elapsed']))
     }, cl = 1)
     out <- rbindlist(out)
     out[, `:=`(n = n, kernel_approx = kernel_approx, kernel_type = kernel_type)]
@@ -97,7 +94,7 @@ if(file.exists(file.path(temp_dir, 'full_bootstrap.rds'))){
 # cBLB SIMULATIONS----
 grid_vals <- as.data.table(expand.grid(n = n_values,
                          subsets = subset_values,
-                         kernel_approx = c(TRUE, FALSE),
+                         kernel_approx = c(TRUE),
                          kernel_type = c('rbfdot', 'vanilladot')))
 grid_vals <- grid_vals[!(kernel_type == 'vanilladot' & kernel_approx)]
 grid_vals[, `:=`(gamma = calculate_gamma(n, subsets))]
@@ -124,8 +121,11 @@ if(file.exists(file.path(temp_dir, 'cblb_bootstrap.rds'))){
     out <- pblapply(seq_len(replications), function(rp){
       set.seed(rp)
       dat <- kangschafer3(n = n, te = te, sigma = sigma, beta_overlap = 0.5)
-      return(causal_blb_stable(data = dat, b = b, subsets = subsets, kernel_approx = kernel_approx,
-                               augment = FALSE, eig_clip = eig_clip, kernel_type = kernel_type))
+      time <- system.time({
+        causal_blb_stable(data = dat, b = b, subsets = subsets, kernel_approx = kernel_approx,
+                          augment = FALSE, eig_clip = eig_clip, kernel_type = kernel_type)
+      })
+      return(data.table(time = time['elapsed']))
     }, cl = 1)
     
     out <- rbindlist(out)
@@ -137,6 +137,5 @@ if(file.exists(file.path(temp_dir, 'cblb_bootstrap.rds'))){
   saveRDS(cblb, file.path(temp_dir, 'cblb_bootstrap.rds'))
 }
 
-full[, .(mean(lower_ci <= te & upper_ci >= te)), by = c('n', 'kernel_approx', 'kernel_type')]
-cblb[, .(mean(lower_ci <= te & upper_ci >= te)), by = c('n', 'subsets', 'gamma', 'kernel_approx',
-                                                        'kernel_type')]
+summary(full$time)
+summary(cblb$time)
