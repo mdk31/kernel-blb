5d4364a8f2a8d30485622862e5bb0b6481880c0d
Matthew Kosko <matthewdkosko@gmail.com>
2025-05-27 21:29:37 -0400

    Added print statements around AIPW checks

--- code/helper_functions.R
+++ code/helper_functions.R
@@ -46,13 +46,13 @@ aipw_kernel_weights <- function(data, Tr, Y, confounder_names, degree1, degree2,
                                  operator=operator),
                               error=function(e) {print(e); NULL})
   # shape
-  str(pyX1); str(pyY1)
+  print(str(pyX1)); print(str(pyY1))
   
   # NA / Inf
-  anyNA(pyX1); any(!is.finite(pyX1))
+  print(anyNA(pyX1)); print(any(!is.finite(pyX1)))
   
   # How many treated
-  table(data[[Tr]])
+  print(table(data[[Tr]]))
   
   #compute K
   matrix_eva <- as.matrix( confounders )
